ba7876d234e79e80e5a33d88886a786b4c69b9ad
Varlen Neto <varlenneto@poli.ufrj.br>
2017-06-15 11:57:51 -0300

    Move pinout definition to header file

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,15 +1,5 @@
 #include <AccelStepper.h>
-
-#define ENABLE_1_GARRAFA 10
-#define ENABLE_2_GARRAFA 11
-#define LED_STATUS       51
-#define BUTTON           53
-#define BUZZER           37
-#define BTN_1            39
-#define BTN_2            41
-#define BTN_3            43
-#define ENDSTOP_1        25
-#define ENDSTOP_2        29
+#include "Pins.h"
 
 AccelStepper motorCopo(4, 2, 3, 4, 5);
 AccelStepper motorGarrafa(4, 6, 7, 8, 9);
---
+++ Firmware/Pins.h
@@ -0,0 +1,15 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define ENABLE_1_GARRAFA 10
+#define ENABLE_2_GARRAFA 11
+#define LED_STATUS       51
+#define BUTTON           53
+#define BUZZER           37
+#define BTN_1            39
+#define BTN_2            41
+#define BTN_3            43
+#define ENDSTOP_1        25
+#define ENDSTOP_2        29
+
+#endif
